ae6992d4d2ca3b5cc4e7b36d9080f9e163f3dfdf
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-09-04 13:55:14 +0600

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import web from '../../assets/img/web.png'
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className="text-white flex justify-between items-center py-20 px-16">
+            <div>
+            <h1 className="text-6xl font-bold font-helvetica">Hi, <br /> I'm Zahid, <br /> Web Developer.</h1>
+            <p className="text-sm text-[#8d8d8d] tracking-[3px] mt-2">Frontend Developer | React JS | Freelancer</p>
+            <Link to="/contact">
+                <button className="px-6 py-2 text-xl uppercase text-[#FFD700] border border-[#FFD700] tracking-[3px] hover:text-[#022C43] hover:bg-[#FFD700] mt-8">contact me</button>
+            </Link>
+            </div>
+            <div>
+                <img src={web} alt="web" />
+            </div>
         </div>
     );
 };
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,30 +11,30 @@ const Sidebar = () => {
         <p className="text-md tracking-wider">ZAHID</p>
       </Link>
       <div className="absolute top-1/3 space-y-6 text-3xl">
-        <Link to="/" className="block">
+        <Link to="/" className="block hover:text-yellow-300">
           <HiOutlineHome></HiOutlineHome>
         </Link>
-        <Link to="/about" className="block">
+        <Link to="/about" className="block hover:text-yellow-300">
           <HiOutlineUser></HiOutlineUser>
         </Link>
-        <Link to="/skills" className="block">
+        <Link to="/skills" className="block hover:text-yellow-300">
           <MdOutlineSettings></MdOutlineSettings>
         </Link>
-        <Link to="/projects" className="block">
+        <Link to="/projects" className="block hover:text-yellow-300">
           <FaRegEye></FaRegEye>
         </Link>
       </div>
       <div className="absolute bottom-4 space-y-6">
-        <Link to="/contact" className="block">
+        <Link to="/contact" className="block hover:text-yellow-300">
           <FaRegEnvelope></FaRegEnvelope>
         </Link>
-        <Link to="" className="block">
+        <Link to="https://www.linkedin.com/in/zahidhasanofficial/" className="block hover:text-yellow-300">
           <FaLinkedin></FaLinkedin>
         </Link>
-        <Link to="" className="block">
+        <Link to="https://github.com/zahidhasan13" className="block hover:text-yellow-300">
           <FaGithub></FaGithub>
         </Link>
-        <Link to="" className="block">
+        <Link to="https://www.facebook.com/zahid.13aug1999/" className="block hover:text-yellow-300">
           <FaFacebook></FaFacebook>
         </Link>
       </div>
